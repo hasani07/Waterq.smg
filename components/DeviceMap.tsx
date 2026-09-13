66f8df11c9dfbd8e43a876173c48f015b45e0413
hasani07 <74604665+hasani07@users.noreply.github.com>
2026-09-13 17:39:36 +0700

    Update DeviceMap.tsx

--- components/DeviceMap.tsx
+++ components/DeviceMap.tsx
@@ -40,7 +40,7 @@ export default function DeviceMap({
     <MapContainer
       center={center}
       zoom={12}
-      style={{ height: "100%", width: "100%", minHeight: "380px" }}
+      style={{ height: "380px", width: "100%" }}
       scrollWheelZoom={false}
     >
       <TileLayer
